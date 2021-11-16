814b4f69be268236df62c029dd271c4c653d365f
AllaSydoriak <alochka12@ukr.net>
2021-11-16 13:19:17 +0200

    fix errors

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <a
+          target="_blank"
+          className="App-link"
+          href="https://reactjs.org"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
